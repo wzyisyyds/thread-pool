bf94ecd8154ae7f9da016128f1a6b82e575ee10a
yexi <yexi@example.com>
2023-04-07 21:23:10 +0800

    1day

--- include/ThreadPool.h
+++ include/ThreadPool.h
@@ -1,8 +1,24 @@
 #ifndef THREAD_POOL_H
 #define THREAD_POOL_H
 
+#include "../include/head.h"
+#include "../include/systemcallAPI.h"
 
 
+typedef struct THREAD_TASK
+{
+    PVOID arg;
+    VOID (*task_fun)(PVOID args);
+    INT_8 task_ready;
+} Task,*Ptr_Task;
+
+
+
+typedef struct THREAD_POOL
+{
+
+
+} Pool,*Ptr_Pool;
 
 
 
---
+++ include/head.h
@@ -0,0 +1,19 @@
+#ifndef HEAD_H
+#define HEAD_H
+
+
+
+typedef signed char             INT_8,      *PINT_8;
+typedef unsigned char           UINT_8,     *PUINT_8;
+typedef signed short int        INT_16,     *PINT_16;
+typedef unsigned short int      UINT_16,    *PUINT_16;
+typedef signed int              INT_32,     *PINT32;
+typedef unsigned int            UINT_32,    *PUINT32;
+typedef signed long long int    INT_64,     *PINT64;
+typedef unsigned long long int  UINT_64,    *PUINT64;
+
+typedef void                    VOID,       *PVOID; 
+
+
+
+#endif
