cea05398900a37630cc73872d220d120f039c84c
yexi <yexi@example.com>
2023-04-18 19:32:06 +0800

    vsfs

--- tests/main.cpp
+++ tests/main.cpp
@@ -13,8 +13,6 @@ int main()
     Init_Thread_Pool_Data data;
     data.Thread_max_size=4;
     data.task_fast_size=16;
-    data.task_fast_size=16;
-    data.task_slow_size=10;
     data.task_slow_size=10;
     data.task_max_size=128;
     PVOID thread_pool=yexi_thread_pool_init(data);
