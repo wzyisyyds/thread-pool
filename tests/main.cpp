0806035c513bf26eb96bdc61c0ff009792d18f11
yexi <yexi@example.com>
2023-04-18 18:47:42 +0800

    dada

--- tests/main.cpp
+++ tests/main.cpp
@@ -15,12 +15,13 @@ int main()
     data.task_fast_size=16;
     data.task_slow_size=10;
     data.task_max_size=128;
+    data.task_max_size=128;
     PVOID thread_pool=yexi_thread_pool_init(data);
     
 
     
 
-    
+
     yexi_thread_pool_free(thread_pool);
     std::cout<<"hello word"<<std::endl;
     return 0;
